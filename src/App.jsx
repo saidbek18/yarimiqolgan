702e8f1a5d1f1141cb8aab124db844cb69da5003
Saidbek <sizningemail@gmail.com>
2025-12-12 23:12:24 +0500

    Fayllarga o‘zgartirishlar kiritildi

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login.jsx";
 import NewLogin from "./NewLogin.jsx";
-import Home from "./HOME.JSX";
+
 
 function App() {
   return (
@@ -11,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/new-login" element={<NewLogin />} />
-        <Route path="/home" element={<Home />} />
         <Route path="*" element={<Login />} /> {/* Default sahifa */}
       </Routes>
     </Router>
